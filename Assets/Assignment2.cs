f3abf8fae75527d0c5cabde29807944a1daf4965
Lavisch <65831893+Lavisch@users.noreply.github.com>
2021-09-14 21:34:46 +0200

    Optimized code in assignment2

--- Assets/Assignment2.cs
+++ Assets/Assignment2.cs
@@ -6,8 +6,8 @@ public class Assignment2 : ProcessingLite.GP21
 {
     public int lines = 8;
     public float startX = 7;
-    public float endX = 12;
     public float startY = 9;
+    public float endX = 12;
     public float endY = 1;
     public float cornerX = 6;
     public float cornerY = 2;
@@ -20,44 +20,26 @@ public class Assignment2 : ProcessingLite.GP21
     // Update is called once per frame
     void Update()
     {
-
         Background(255, 255, 255);
 
         for (int i = 0; i < lines; i++)
         {
-            //Working code
-            /*
-            float offsetXStart = (cornerX - startX) * i / lines;
-            float offsetXEnd = (endX - cornerX) * i / lines;
-            float offsetYStart = (cornerY - startY) * i / lines;
-            float offsetYEnd = (endY - cornerY) * i / lines;
-            */
+            float x1 = startX + ((cornerX - startX) * i / lines);
+            float y1 = startY + ((cornerY - startY) * i / lines);
+            float x2 = cornerX + ((endX - cornerX) * (i + 1) / lines);
+            float y2 = cornerY + ((endY - cornerY) * (i + 1) / lines);
 
-            float offsetXStart = (cornerX - startX) * i / lines;
-            float offsetXEnd = (endX - cornerX) * (i + 1) / lines;
-            float offsetYStart = (cornerY - startY) * i / lines;
-            float offsetYEnd = (endY - cornerY) * (i + 1) / lines;
-
-            if (i % 3 == 0)
-            {
-                Stroke(128, 128, 128);
-            }
-            else
-            {
-                Stroke(0, 0, 0);
-            }
-
-            Line
-            (
-                startX + offsetXStart,
-                startY + offsetYStart,
-                cornerX + offsetXEnd,
-                cornerY + offsetYEnd
-            );
+            Stroke(128, 128, 128);
             Circle(startX, startY, 0.2f);
             Circle(cornerX, cornerY, 0.2f);
             Circle(endX, endY, 0.2f);
 
+            if (i % 3 == 0)
+            {
+                Stroke(0, 0, 0);
+            }
+
+            Line(x1, y1, x2, y2);
         }
     }
 }
